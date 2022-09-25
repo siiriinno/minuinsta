4d867af49490eed5e239b3a231f7747885bb96cd
siiriinno <siiri.inno@tlu.ee>
2022-09-25 10:37:00 +0300

    Kasutaja muutmise lisamine.

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,86 @@ app.get("/users/:id", (req: Request, res: Response) => {
     }
 })
 
-//todo kasutaja lisamine
+app.post("/users", (req: Request, res: Response) => {
+    const {name, username, genderID, bio} = req.body;
+    const id = users.length + 1;
+    if (name && username && genderID && bio) {
+        const newUser: User = {
+            id,
+            name,
+            username,
+            genderID,
+            bio,
+        };
+        users.push(newUser);
+
+        res.status(201).json({
+            success: true,
+            message: `User with id ${newUser.id} created`,
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: `Invalid request`,
+        });
+    }
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    const uid = parseInt(req.params.id);
+    const index = users.findIndex((u) => u.id === uid);
+    if (index < 0) {
+        res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    } else {
+        const {name, genderID, bio, email, password, profileImageUrl, website } = req.body;
+        if (name) {
+            users[index].name = name;
+        }
+        if (genderID) {
+            users[index].genderID = genderID;
+        }
+        if (email) {
+            users[index].email = email;
+        }
+        if (password) {
+            users[index].password = password;
+        }
+        if (profileImageUrl) {
+            users[index].profileImageUrl = profileImageUrl;
+        }
+        if (website) {
+            users[index].website = website;
+        }
+        if (bio) {
+            users[index].bio = bio;
+        }
+        res.status(200).json({
+            success: true,
+            message: `User with id ${uid} updated`,
+        });
+    }
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    const uid = parseInt(req.params.id);
+    const index = users.findIndex((u) => u.id === uid);
+    if (index < 0) {
+        res.status(404).json({
+            success: false,
+            message: `User not found`,
+        });
+    } else {
+        users.splice(index, 1);
+        res.status(201).json({
+            success: true,
+            message: `User deleted`,
+        });
+    }
+});
+
 
 app.listen(3000, () => {
     console.log("Server töötab");
